b2d0c84ea3a9f40a0d832801e3448e1274b1802c
mtalha987 <miantalhapaf@gmail.com>
2025-01-27 11:55:30 +0500

    Category arrow functionality added

--- app/Components/product/ProductComplete.jsx
+++ app/Components/product/ProductComplete.jsx
@@ -9,9 +9,15 @@ import ProductDataNew from "./ProductDataNew";
 const ProductComplete = () => {
 
   const [selectedCategory, setSelectedCategory] = useState("All Products");
+  const [isDropdownOpen, setIsDropdownOpen] = useState(true);
+
 
   const handleCategorySelect = (category) => {
     setSelectedCategory(category);
+  };
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
   };
   return (
     <>
@@ -25,42 +31,29 @@ const ProductComplete = () => {
                 placeholder="Search"
               />
             </form>
-            <div className="flex items-center content-center justify-between pt-6 mx-4">
+            <div
+              className="flex items-center content-center justify-between pt-6 mx-4 ">
               <h2 className="text-divi-gray text-2xl font-normal font-nohemi leading-[28.80px]">
                 Categories
               </h2>
-              <div>
-                <Image src={ArrowDown} alt="Arrow Down" />
+              <div onClick={toggleDropdown} className="cursor-pointer">
+                <Image src={ArrowDown} className={`${isDropdownOpen ? "" : "rotate-180"}`}   alt="Arrow" />
               </div>
             </div>
-            <div className="w-full h-[0px] border border-[#eae9e8] mt-6"></div>
-            {/* Categories List Call */}
-            <CategoryList  onCategorySelect={handleCategorySelect} selectedCategory={selectedCategory} />
+            {isDropdownOpen && (
+              <>
+                <div className="w-full h-[0px] border border-[#eae9e8] mt-6"></div>
+                {/* Categories List Call */}
+                <CategoryList
+                  onCategorySelect={handleCategorySelect}
+                  selectedCategory={selectedCategory}
+                />
+              </>
+            )}
           </div>
-        </div>
+          </div>
         <div className="xl:w-[70%] md:w-[74%] w-full">
-          {/* <ProductData gridSizeL={3} gridSizeM={2} gridSizeS={2} limit={12}  selectedCategory={selectedCategory}/> */}
           <ProductDataNew selectedCategory={selectedCategory} />
-          {/* <div className="flex gap-2 justify-center lg:mt-16 mt-5 lg:mb-20 mb-5 ">
-            <div className="w-10 h-10 bg-white shadow flex justify-center items-center rotate-90">
-              <Image src={ArrowDown} alt="Arrow Down" />
-            </div>
-            <div className="w-10 h-10  shadow flex justify-center items-center bg-primary text-[white] text-base font-normal font-primary leading-normal">
-              1
-            </div>
-            <div className="w-10 h-10 bg-white shadow flex justify-center items-center text-[#554e49] text-base font-normal font-primary leading-normal">
-              2
-            </div>
-            <div className="w-10 h-10 bg-white shadow flex justify-center items-center text-[#554e49] text-base font-normal font-primary leading-normal">
-              3
-            </div>
-            <div className="w-10 h-10 bg-white shadow flex justify-center items-center text-[#554e49] text-base font-normal font-primary leading-normal">
-              4
-            </div>
-            <div className="w-10 h-10 bg-white shadow flex justify-center items-center -rotate-90">
-              <Image src={ArrowDown} alt="Arrow Down" />
-            </div>
-          </div> */}
         </div>
       </div>
     </>
