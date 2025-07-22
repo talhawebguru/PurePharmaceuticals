509eb17876beb59a117bb89456f8f297d2f7e5ae
mtalha987 <miantalhapaf@gmail.com>
2025-07-22 12:34:32 +0500

    Pure pharma done now just testing and improvement remaining

--- app/services/api.js
+++ app/services/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL, 
   headers: {
     'Content-Type': 'application/json',
-    'ngrok-skip-browser-warning': 'true'
+    // 'ngrok-skip-browser-warning': 'true'
   },
 });
 
