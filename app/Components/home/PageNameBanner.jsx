a429ba8236ec4adee55c24142ae1c960890c17fc
mtalha987 <miantalhapaf@gmail.com>
2025-08-01 16:11:02 +0500

    h1 setting on all pages

--- app/Components/home/PageNameBanner.jsx
+++ app/Components/home/PageNameBanner.jsx
@@ -21,14 +21,14 @@ const PageNameBanner = (props) => {
           quality={85}
         />
       </motion.div>
-      <motion.h2
+      <motion.h1
         className="text-white font-bold shadow-md bg-black/50 px-4 py-2 rounded inline-block text-5xl xs:text-[32px] font-nohemi absolute bottom-9 left-9 z-10"
         initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8, delay: 0.2 }}
       >
         {props.title}
-      </motion.h2>
+      </motion.h1>
     </div>
   );
 };
